1da5941772a21dc36ee9721fa2428696b2acc7ae
Soriano-Trigueros <44211245+Soriano-Trigueros@users.noreply.github.com>
2025-11-12 11:02:11 +0000

    Deploying to gh-pages from @ Soriano-Trigueros/Soriano-Trigueros.github.io@3f75cdddd5f8b09866f9e463902151625487a13a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-docencia",
-          title: "Docencia",
+          title: "docencia",
           description: "",
           section: "Navigation",
           handler: () => {
